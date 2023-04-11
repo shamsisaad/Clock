3c8117722ef328b21c8fdbb06ed6b1fd36e3428a
shamsisaad <engrsaadshamsi@gmail.com>
2023-04-11 15:30:42 +0500

    Linked Html element into Java file

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+var hoursElem = document.getElementById("hours");
+var minutesElem = document.getElementById("minutes");
+var secondsElem = document.getElementById("seconds");
+var amPmElem = document.getElementById("amPm");
+

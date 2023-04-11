b1f1a5a88093273361f8293fc3c848f7d1fdce9d
shamsisaad <engrsaadshamsi@gmail.com>
2023-04-11 15:32:16 +0500

    Updated app.js
    set time function for digital clock

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ var minutesElem = document.getElementById("minutes");
 var secondsElem = document.getElementById("seconds");
 var amPmElem = document.getElementById("amPm");
 
+setInterval(function() {
+    let date = new Date();  
+    let hours = date.getHours();
+    let amPm = "AM";
+      if(hours > 11){
+          amPm ="PM";
+      if (hours > 12) {
+      hours -= 12;
+      }
+   }  
+   hoursElem.innerText = hours();
+   minutesElem.innerText = date.getMinutes();
+   secondsElem.innerText = date.getSeconds();
+   amPmElem.innerText = date.getSeconds();
+  }, 1000);

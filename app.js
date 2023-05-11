cff72ad061d2a16194b064fff3b70dfa56915d9a
shamsisaad <engrsaadshamsi@gmail.com>
2023-05-12 00:19:32 +0500

    setting AM / PM

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ var secondsElem = document.getElementById("seconds");
 var amPmElem = document.getElementById("amPm");
 
 setInterval(function() {
-    let date = new Date();  
-    let hours = date.getHours();
-    let amPm = "AM";
-      if(hours > 11){
-          amPm ="PM";
-      if (hours > 12) {
-      hours -= 12;
-      }
-   }  
-   hoursElem.innerText = hours();
+    var date = new Date();  
+    var hours = date.getHours();
+   
+   hoursElem.innerText = date.getHours();
    minutesElem.innerText = date.getMinutes();
    secondsElem.innerText = date.getSeconds();
-   amPmElem.innerText = date.getSeconds();
   }, 1000);
+
+  if(hours > 12){
+    amPm.innerText ="PM";
+}else {amPm.innerText = "AM";
+}    
+if (hours > 12) {
+hoursElem.innerText -= 12;
+}
